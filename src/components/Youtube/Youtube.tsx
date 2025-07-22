e1c2feccf2606762d7bd31c6784ef995f353708c
Henrique <silva@iMac-Pro-de-Henrique.local>
2025-07-22 09:17:07 -0300

    Feat: add new components Youtube Format

--- src/components/Youtube/Youtube.tsx
+++ src/components/Youtube/Youtube.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { AbsoluteFill, Sequence, Audio, staticFile } from "remotion";
 import { FadeTransition } from "../transitions/FadeTransition";
 import { CoverDefault } from "../VideoDefault/CoverDefault";
-import { VideosSequence } from "../VideoDefault/VideosSequence";
 
 type Scene = {
   id: string;
@@ -17,7 +16,6 @@ type YoutubeTemplateProps = {
   coverDurationInFrames: number;
 };
 
-// Dentro do YoutubeTemplate:
 export const YoutubeTemplate: React.FC<YoutubeTemplateProps> = ({
   scenes,
   musicStartAtFrame,
@@ -25,18 +23,6 @@ export const YoutubeTemplate: React.FC<YoutubeTemplateProps> = ({
   coverDurationInFrames,
 }) => {
   let fromFrame = 0;
-
-  // soma total dos frames das cenas já passadas
-  scenes.forEach((scene) => {
-    fromFrame += scene.durationInFrames;
-  });
-
-  // Exemplo dos vídeos que você quer passar para sequência
-  const videosToPlay = [
-    { id: "vid1", src: "/videos/10.mp4", durationInFrames: 90 },
-    { id: "vid2", src: "/videos/05.mp4", durationInFrames: 120 },
-    { id: "vid3", src: "/videos/03.mp4", durationInFrames: 80 },
-  ];
 
   return (
     <AbsoluteFill style={{ backgroundColor: "black" }}>
@@ -57,14 +43,9 @@ export const YoutubeTemplate: React.FC<YoutubeTemplateProps> = ({
         );
       })}
 
-      {/* Capa */}
+      {/* Capa sobreposta antes do final da intro */}
       <Sequence from={coverStartFrame} durationInFrames={coverDurationInFrames}>
         <CoverDefault />
-      </Sequence>
-
-      {/* Sequência de vídeos logo após a capa */}
-      <Sequence from={coverStartFrame + coverDurationInFrames}>
-        <VideosSequence videos={videosToPlay} fadeDurationInFrames={20} />
       </Sequence>
 
       {/* Música sincronizada com a capa */}
