84c93bf7a4643cf0cb64b67cef4ed6719496ef7c
Henrique Silva <jhenrique367@gmail.com>
2025-07-23 18:15:12 -0300

    Chore: adjusted dinamic component

--- src/components/Youtube/PauseableVideo.tsx
+++ src/components/Youtube/PauseableVideo.tsx
@@ -5,57 +5,68 @@ type SplitVideoWithPauseProps = {
   src: string;
   pauseFrame: number;
   pauseDuration: number;
+  startFrom: number;
   freezeFrame: number;
   totalDuration: number;
   zoomStartFrame: number;
   zoomEndFrame: number;
+  zoomFrom: number;
+  zoomTo: number;
 };
+
 
 export const PauseableVideo: React.FC<SplitVideoWithPauseProps> = ({
   src,
   pauseFrame,
   pauseDuration,
+  startFrom,
   freezeFrame,
-  totalDuration,
   zoomStartFrame,
   zoomEndFrame,
+  zoomFrom,
+  zoomTo
 }) => {
   return (
     <AbsoluteFill style={{ position: 'relative' }}>
+      {/* Vídeo normal com zoom animado */}
       <ZoomEffect
         zoomStartFrame={zoomStartFrame}
         zoomEndFrame={zoomEndFrame}
-        zoomFrom={1}
-        zoomTo={2}
+        zoomFrom={zoomFrom}
+        zoomTo={zoomTo}
       >
+
         <Video
           src={src}
+          startFrom={startFrom}
           volume={0}
           style={{ width: '100%', height: '100%', objectFit: 'cover' }}
         />
       </ZoomEffect>
 
-      <Sequence from={pauseFrame} durationInFrames={pauseDuration}>
-        <ZoomEffect
-          zoomStartFrame={zoomStartFrame}
-          zoomEndFrame={zoomEndFrame}
-          zoomFrom={1}
-          zoomTo={2}
-        >
-          <Freeze frame={freezeFrame}>
-          <Video
-              src={src}
-              startFrom={freezeFrame}
-              volume={0}
-              onError={(error) => {
-                console.error('Erro no vídeo Freeze:', error);
-              }}
-              style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-            />
-
-          </Freeze>
-        </ZoomEffect>
-      </Sequence>
+      {/* Pausa congelada com o mesmo zoom aplicado */}
+      {pauseDuration > 0 && (
+        <Sequence from={pauseFrame} durationInFrames={pauseDuration}>
+         <ZoomEffect
+            zoomStartFrame={zoomStartFrame}
+            zoomEndFrame={zoomEndFrame}
+            zoomFrom={zoomFrom}
+            zoomTo={zoomTo}
+          >
+            <Freeze frame={freezeFrame}>
+              <Video
+                src={src}
+                startFrom={freezeFrame}
+                volume={0}
+                onError={(error) => {
+                  console.error('Erro no vídeo Freeze:', error);
+                }}
+                style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+              />
+            </Freeze>
+          </ZoomEffect>
+        </Sequence>
+      )}
     </AbsoluteFill>
   );
 };
