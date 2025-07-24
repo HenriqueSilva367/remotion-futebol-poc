b5f541d7769421439c7655202a7aa09b7340289a
Henrique Silva <jhenrique367@gmail.com>
2025-07-24 12:03:24 -0300

    Feat: add new configurate

--- src/config/videoConfig.ts
+++ src/config/videoConfig.ts
@@ -1,56 +1,56 @@
 export const videoConfig = {
-    fps: 30,
-  
-    durations: {
-      intro: 196,
-      cover: 90,
-      sequence: 4590,
-      freezeDuration: 100,
-    },
-  
-    zoom: {
-      from: 1,
-      to: 2,
-      startFrame: 100,
-      endFrame: 200,
-    },
-  
-    music: {
-      src: 'audios/music.mp3',
-      volume: 0.8,
-      startFrame: 177,
-    },
-  
-    cover: {
-      startFrame: 165,
-      durationInFrames: 90,
-    },
-  
-    videoCuts: [
-      {
-        id: 'clip-1',
-        src: '/videos/gol.mp4',
-        startFrom: 0,
-        durationInFrames: 210,
-  
-        pause: {
-          timelineFrame: 165,    // onde na timeline global inicia a pausa
-          duration: 45,          // duração da pausa em frames
-          freezeFrame: 225,      // frame específico do vídeo a ser congelado
-        },
-        zoom: {
-          from: 1,
-          to: 2,
-          startFrame: 100,
-          endFrame: 200,
-        },
+  fps: 30,
+
+  durations: {
+    intro: 196,
+    cover: 90,
+    sequence: 4590,
+    freezeDuration: 100,
+  },
+
+  music: {
+    src: 'audios/music.mp3',
+    volume: 0.8,
+    startFrame: 177,
+  },
+
+  cover: {
+    startFrame: 165,
+    durationInFrames: 90,
+  },
+
+  videoCuts: [
+    {
+      id: 'clip-1',
+      src: '/videos/gol.mp4',
+      startFrom: 0,
+      durationInFrames: 210,
+
+      pause: {
+        timelineFrame: 165,
+        duration: 45,
+        freezeFrame: 225,
+        zoomPauseTo: 2, 
       },
-      {
-        id: 'clip-2',
-        src: '/videos/gol.mp4',
-        startFrom: 159,
-        durationInFrames: 400,
+
+      zoom: {
+        from: 1,
+        to: 1.5,
+        startFrame: 100,
+        endFrame: 200,
       },
-    ],
-  };
-  
+    },
+    {
+      id: 'clip-2',
+      src: '/videos/gol.mp4',
+      startFrom: 159,
+      durationInFrames: 400,
+      zoom: {
+        from: 2,
+        to: 1.5,
+        startFrame: 100,
+        endFrame: 200,
+      },
+    },
+  ],
+};
