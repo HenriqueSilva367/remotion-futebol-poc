c2ed4f83c3bd68b3a96b1ab3efe8f5c166fc60d2
Henrique Silva <jhenrique367@gmail.com>
2025-08-05 16:48:36 -0300

    Feat: remover comments

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,10 +3,7 @@
 import { Composition } from "remotion";
 import { MyComp } from "./components/MyComp";
 import { FinalVideo } from "./components/FullVideo";
-import { IntroVideo } from "./components/VideoDefault/IntroVideo";
-import { CoverDefault } from "./components/VideoDefault/CoverDefault";
 import { Videos } from "./components/Youtube/Video";
-
 
 export const RemotionRoot: React.FC = () => {
   return (
@@ -14,57 +11,21 @@ export const RemotionRoot: React.FC = () => {
       <Composition
         id="InstagramVideo"
         component={MyComp}
-        durationInFrames={300} // 10 segundos a 30fps
+        durationInFrames={300} 
         fps={30}
         width={1080}
         height={1920}
         defaultProps={{}}
       />
-      {/* 
-      <Composition
-        id="Embaixadinhas"
-        component={JugglingCounter} 
-        durationInFrames={300 * 60} // 10 segundos
-        fps={30}
-        width={1080}
-        height={1920}
-        defaultProps={{
-          totalJuggles: 15, // Total de embaixadinhas
-        }}
-      /> */}
-
-      <Composition
-        id="IntroVideo"
-        component={IntroVideo}
-        durationInFrames={100 * 30} // duração da intro (6 segundos a 30fps)
-        fps={30}
-        width={1920}
-        height={1080}
-        defaultProps={{
-          videoFileName: "/videos/intro.mp4", // arquivo colocado em /public
-          backgroundColor: "#000000",
-          muted: false, // áudio da intro habilitado
-        }}
-      />
-
+ 
       <Composition
         id="Full-video"
         component={FinalVideo}
-        durationInFrames={100 * 30} // 100 segundos * 30fps = 3000 frames
+        durationInFrames={100 * 30}
         fps={30}
         width={1080}
         height={1920}
       />
-
-      <Composition
-        id="CoverDefault"
-        component={CoverDefault}
-        durationInFrames={100 * 4}
-        fps={30}
-        width={1080}
-        height={1920}
-      />
-
       <Composition
         id="YoutubeTemplate"
         component={Videos}
@@ -72,6 +33,15 @@ export const RemotionRoot: React.FC = () => {
         fps={30}
         width={1920}
         height={1080}
+      />
+
+      <Composition
+        id="InstagramTemplate"
+        component={Videos}
+        durationInFrames={100 * 30}
+        fps={30}
+        width={1080}
+        height={1920}
       />
     </>
   );
