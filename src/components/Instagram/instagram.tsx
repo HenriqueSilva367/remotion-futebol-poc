ef08a58ca3c12beaae8f3397beed2aee48ad5101
Henrique Silva <jhenrique367@gmail.com>
2025-08-15 16:37:31 -0300

    Feat: add new components and adjusted code

--- src/components/Instagram/instagram.tsx
+++ src/components/Instagram/instagram.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { AbsoluteFill, Sequence } from "remotion";
-import { GifEffect } from "../transitions/GifEffect";
-import { buildTimeline } from "../../utils/buildTimeline";
-import { videoConfig } from "../../config/videoConfig";
+import { AbsoluteFill, Sequence, Video } from "remotion";
 import { LogoFooter } from "../transitions/LogoFooter";
 import { BackgroundMusic } from "../BackgroundMusic";
+import { MultiVideoLayout } from "./components/MuiltVideosLayout";
+import { resolveAssetPath } from "../../config/resolveAssetPath";
+import { BlackAndWhite } from "./components/BalckAndWhite";
+import { SlowMotionVideo } from "./components/SlowMotion";
+import { videoConfigInsta } from "../../config/videoConfigInsta";
 
 type Scene = {
   id: string;
@@ -12,21 +14,10 @@ type Scene = {
   durationInFrames: number;
 };
 
-type LogoFooterEffect = {
-  startFrame: number;
-  durationInFrames: number;
-  x?: number;
-  y?: number;
-  width?: number;
-  height?: number;
-};
-
 type InstagramTemplateProps = {
   scenes: Scene[];
   musicStartAtFrame: number;
   musicDuration: number;
-  coverStartFrame: number; 
-  coverDurationInFrames: number;
 };
 
 export const InstagramTemplate: React.FC<InstagramTemplateProps> = ({
@@ -34,42 +25,123 @@ export const InstagramTemplate: React.FC<InstagramTemplateProps> = ({
   musicStartAtFrame,
   musicDuration,
 }) => {
-  const timeline = buildTimeline(scenes);
+  let currentFrame = 0;
 
   return (
     <AbsoluteFill style={{ backgroundColor: "black" }}>
-      {timeline.map((scene) => (
-        <Sequence
-          key={scene.id}
-          from={scene.from}
-          durationInFrames={scene.durationInFrames}
-        >
-          {scene.component}
-        </Sequence>
-      ))}
 
-      {/* AUDIO */}
+      {scenes
+        .filter((scene) => 'text' in scene.component.props)
+        .map((scene) => {
+          const seq = (
+            <Sequence
+              key={scene.id}
+              from={currentFrame}
+              durationInFrames={scene.durationInFrames}
+            >
+              {scene.component}
+            </Sequence>
+          );
+          currentFrame += scene.durationInFrames;
+          return seq;
+        })}
+
+      {videoConfigInsta.multiVideos?.length > 0 && (
+        <Sequence
+          from={currentFrame}
+          durationInFrames={videoConfigInsta.multiVideos.reduce(
+            (acc, vid) => acc + (vid.durationInFrames ?? 180),
+            0
+          )}
+        >
+          <MultiVideoLayout videos={videoConfigInsta.multiVideos} />
+        </Sequence>
+      )}
+      {currentFrame += videoConfigInsta.multiVideos.reduce(
+        (acc, vid) => acc + (vid.durationInFrames ?? 180),
+        0
+      )}
+
+   
+      {videoConfigInsta.videoCuts.map((cut) => {
+        const seq = (
+          <Sequence
+            key={cut.id}
+            from={currentFrame}
+            durationInFrames={cut.durationInFrames}
+          >
+            {(() => {
+            
+              let videoElement: React.ReactNode = (
+                <Video
+                  src={resolveAssetPath(cut.src)}
+                  startFrom={cut.startFrom ?? 0}
+                  volume={cut.volume ?? 1}
+                  style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                />
+              );
+
+              if (
+                cut.slowMotionStart !== undefined &&
+                cut.slowMotionEnd !== undefined &&
+                cut.slowMotionFactor !== undefined
+              ) {
+                videoElement = (
+                  <SlowMotionVideo
+                    src={resolveAssetPath(cut.src)}
+                    startFrom={cut.startFrom ?? 0}
+                    durationInFrames={cut.durationInFrames}
+                    slowStart={cut.slowMotionStart}
+                    slowEnd={cut.slowMotionEnd}
+                    slowFactor={cut.slowMotionFactor}
+                    volume={cut.volume ?? 1}
+                  />
+                );
+              }
+
+              if (
+                cut.blackAndWhiteStart !== undefined &&
+                cut.blackAndWhiteEnd !== undefined
+              ) {
+                videoElement = (
+                  <BlackAndWhite
+                    startFrame={cut.blackAndWhiteStart}
+                    endFrame={cut.blackAndWhiteEnd}
+                  >
+                    {videoElement}
+                  </BlackAndWhite>
+                );
+              }
+
+              return videoElement;
+            })()}
+          </Sequence>
+        );
+
+        currentFrame += cut.durationInFrames;
+        return seq;
+      })}
+
       <Sequence from={musicStartAtFrame} durationInFrames={musicDuration}>
         <BackgroundMusic
-          src={videoConfig.music.src}
+          src={videoConfigInsta.music.src}
           startAt={0}
           durationInFrames={musicDuration}
-          fadeInDuration={videoConfig.music.fadeInDuration}
-          fadeOutDuration={videoConfig.music.fadeOutDuration}
-          useFade={videoConfig.music.useFade}
-          maxVolume={videoConfig.music.volume}
+          fadeInDuration={videoConfigInsta.music.fadeInDuration}
+          fadeOutDuration={videoConfigInsta.music.fadeOutDuration}
+          useFade={videoConfigInsta.music.useFade}
+          maxVolume={videoConfigInsta.music.volume}
         />
       </Sequence>
-
-      {/* EFEITOS GIF DINÂMICOS */}
-      {videoConfig.gifEffects?.map((effect, i) => (
-        <GifEffect key={i} {...effect} />
-      ))}
-
-      {/* LOGO FOOTER */}
-      {videoConfig.logoFooters?.map((effect: LogoFooterEffect, i: number) => (
+      
+      {videoConfigInsta.logoFooters?.map((effect, i) => (
         <LogoFooter key={i} {...effect} />
       ))}
+
+      {videoConfigInsta.logoFooters?.map((effect, i) => (
+        <LogoFooter key={i} {...effect} />
+      ))}
+
     </AbsoluteFill>
   );
 };
