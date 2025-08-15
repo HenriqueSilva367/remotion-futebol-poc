fb7f42287dd42b9dd7793203b5746419a1a91da1
Henrique Silva <jhenrique367@gmail.com>
2025-08-15 16:33:30 -0300

    Feat: add new component Typewrite

--- src/components/Typewriter.tsx
+++ src/components/Typewriter.tsx
@@ -16,10 +16,8 @@ export const Typewriter: React.FC<TypewriterProps> = ({
 }) => {
   const frame = useCurrentFrame();
 
-  // calcula a proporção da animação (de 0 a 1)
   const progress = Math.min(frame / durationInFrames, 1);
 
-  // calcula quantas letras mostrar
   const lettersToShow = Math.floor(progress * text.length);
 
   const displayedText = text.substring(0, lettersToShow);
