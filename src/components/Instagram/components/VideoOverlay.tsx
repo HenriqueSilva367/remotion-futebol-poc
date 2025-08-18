f10185166bc11f33a8c20599b7ad9ffebcef7490
Henrique Silva <jhenrique367@gmail.com>
2025-08-18 16:52:08 -0300

    Feat: adjusted componet and new video

--- src/components/Instagram/components/VideoOverlay.tsx
+++ src/components/Instagram/components/VideoOverlay.tsx
@@ -1,38 +1,58 @@
-import { Sequence, staticFile, Video } from "remotion";
+import { AbsoluteFill, Sequence, Video, staticFile } from "remotion";
 
-type VideoOverlayProps = {
+type BackgroundConfig = {
+  src: string;
   startFrame: number;
   durationInFrames: number;
+  volume?: number;
+};
+
+type OverlayConfig = {
   src: string;
+  startFrame: number;
+  durationInFrames: number;
   x?: number;
   y?: number;
   width?: number | string;
   height?: number | string;
+  volume?: number;
 };
 
-export const VideoOverlay: React.FC<VideoOverlayProps> = ({
-  startFrame,
-  durationInFrames,
-  src,
-  x = 100,
-  y = 100,
-  width = 200,
-  height = 200,
+type VideoWithOverlayProps = {
+  background: BackgroundConfig;
+  overlay: OverlayConfig;
+};
+
+export const VideoWithOverlay: React.FC<VideoWithOverlayProps> = ({
+  background,
+  overlay,
 }) => {
   return (
-    <Sequence from={startFrame} durationInFrames={durationInFrames}>
-      <Video
-        src={staticFile(src)}
-        style={{
-          position: "absolute",
-          left: x,
-          top: y,
-          width,
-          height,
-          zIndex: 9999,
-          pointerEvents: "none",
-        }}
-      />
-    </Sequence>
+    <AbsoluteFill>
+ 
+      <Sequence from={background.startFrame} durationInFrames={background.durationInFrames}>
+        <Video
+          src={staticFile(background.src)}
+          volume={background.volume ?? 1}
+          style={{ width: "100%", height: "100%", objectFit: "cover" }}
+        />
+      </Sequence>
+
+      <Sequence from={overlay.startFrame} durationInFrames={overlay.durationInFrames}>
+        <Video
+          src={staticFile(overlay.src)}
+          volume={overlay.volume ?? 1}
+          style={{
+            position: "absolute",
+            left: overlay.x ?? 0,
+            top: overlay.y ?? 0,
+            width: overlay.width ?? 300,
+            height: overlay.height ?? 200,
+            objectFit: "cover",
+            zIndex: 999,
+          }}
+        />
+      </Sequence>
+    </AbsoluteFill>
   );
 };
